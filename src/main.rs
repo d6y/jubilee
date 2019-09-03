3b7017456d6e97a309037f7776593d29b3873164
Richard Dallaway <richard@dallaway.com>
2019-09-03 23:00:32 +0100

    cargo fmt and clippy

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,5 @@ fn main() -> Result<(), std::io::Error> {
     let html = fs::read_to_string("data/one-result.html")?;
     let maybe_available = BookAvailability::from_html(&html);
     println!("{:?}", maybe_available);
-    Ok( () )
+    Ok(())
 }
-
--- src/parse.rs
+++ src/parse.rs
@@ -2,7 +2,7 @@ use soup::prelude::*;
 
 #[derive(Debug, PartialEq)]
 pub struct BookAvailability {
-    location: String, // e.g., shelf number
+    location: String,     // e.g., shelf number
     availability: String, // e.g., on loan
 }
 
@@ -10,33 +10,40 @@ impl BookAvailability {
     // Implementation for the SPYDUS html output (version 9?)
     pub fn from_html(html: &str) -> Option<BookAvailability> {
         let soup = Soup::new(&html);
-        
+
         // The <div> with class holdings is a table of title availability (if any)
         let section = soup.class("holdings").find()?;
 
-        // Inside that div, the 3rd and 4th TDs (actually across 2 TRs) gives the 
+        // Inside that div, the 3rd and 4th TDs (actually across 2 TRs) gives the
         // location and availability text description
-        let book_cells: Vec<String> = section.tag("td").find_all().skip(2).take(2).map(|cell| cell.text()).collect();
+        let book_cells: Vec<String> = section
+            .tag("td")
+            .find_all()
+            .skip(2)
+            .take(2)
+            .map(|cell| cell.text())
+            .collect();
 
         match &book_cells[..] {
-            [loc, available] => Some(BookAvailability { 
-                location: loc.trim().to_string(), 
-                availability: available.trim().to_string() 
-                }),
+            [loc, available] => Some(BookAvailability {
+                location: loc.trim().to_string(),
+                availability: available.trim().to_string(),
+            }),
             _ => None,
         }
     }
 }
 
+#[cfg(test)]
 mod test {
-    use super::BookAvailability;
     use std::fs;
+    use super::BookAvailability;
     #[test]
     fn test_parse_one_result() {
-        let expected = BookAvailability { 
-            location: String::from("599.938"), 
-            availability: String::from("Reservation Allocation Expired (BHCC) (Set: 08 May 2019)") 
-         };
+        let expected = BookAvailability {
+            location: String::from("599.938"),
+            availability: String::from("Reservation Allocation Expired (BHCC) (Set: 08 May 2019)"),
+        };
         let html = fs::read_to_string("data/one-result.html").expect("HTML missing?");
         assert_eq!(BookAvailability::from_html(&html), Some(expected));
     }
